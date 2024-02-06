da2c741490dfda3db680d80f502dc92502100af8
Alseïdon <alexandre.prieur@obspm.fr>
2024-02-06 15:07:44 +0100

    Added multiple dimensions integration

--- src/integration.jl
+++ src/integration.jl
@@ -1,5 +1,6 @@
 using .TDevCore
 
+# SINGLE VARIABLE
 function taylor_method_step!(f::Function, initial_pt::Number,
     initial_t::Number, final_t::Number,
     err::Number, ord::Int)
@@ -29,7 +30,47 @@ function taylor_method(f::Function, initial_pt::Number,
     for i in 1:max_steps
         pt, t, is_over = taylor_method_step!(f, pt, t, final_t, err, ord)
         if is_over
-            @info "finished" i
+            return pt
+        end
+    end
+    @error "couldn't finish" pt t
+    @assert false
+end
+
+
+# MULTIPLE VARIABLES
+function taylor_method_step!(f::Function, initial_pt::AbstractVector,
+    initial_t::Number, final_t::Number,
+    err::Number, ord::Int)
+    x = to_tdev.(initial_pt, ord)
+    aux = zero.(x)
+    for i in 1:ord
+        aux = f(x, initial_t)
+        for j in eachindex(x)
+            x[j].dev[i+1] = aux[j].dev[i] / i
+        end
+    end
+    max_step = final_t - initial_t
+    if max_step == 0
+        return compute.(x, 0), initial_t, true
+    end
+    # TODO compute order
+    step = (err / abs(sum(map(c->c.dev[ord+1], x))))^(1 / ord) * sign(max_step)
+    if abs(step) > abs(max_step)
+        return compute.(x, max_step), initial_t + max_step, true
+    else
+        return compute.(x, step), initial_t + step, false
+    end
+end
+
+function taylor_method(f::Function, initial_pt::AbstractVector,
+    initial_t::Number, final_t::Number;
+    err::Number=1e-16, ord::Int=15, max_steps::Int=100)
+    pt = copy(initial_pt)
+    t = initial_t
+    for i in 1:max_steps
+        pt, t, is_over = taylor_method_step!(f, pt, t, final_t, err, ord)
+        if is_over
             return pt
         end
     end
